98c52f69ddf38415287445eeee5bca28c480de85
omidSharifi <sharifidavood743@gmail.com>
2021-11-12 17:10:12 +0330

    getData

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import SearchBar from '../Components/SearchBar';
+import Coinlist from '../Components/Coinlist';
 
-export default function Home() {
+export default function Home({filteredCoins}) {
+  console.log(filteredCoins);
   return (
     <div>
       <Head>
@@ -13,8 +15,29 @@ export default function Home() {
       </Head>
 
      <SearchBar/>
+     <Coinlist filteredCoins={filteredCoins} />
+
 
       
     </div>
   )
+
+
+
+
+
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  );
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins
+    }
+  };
+};
